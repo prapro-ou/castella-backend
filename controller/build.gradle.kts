58d1ed7971a83566dc7c4f0b1e9adc8f457828a9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-02 11:33:03 +0900

    [delete] controllerのbuild.gradleから余計なコードを削除

--- controller/build.gradle.kts
+++ controller/build.gradle.kts
@@ -3,15 +3,6 @@ plugins {
     id("io.ktor.plugin") version "2.3.2"
 }
 
-group = "com.vb4"
-version = "0.0.1"
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
 dependencies {
     implementation(libs.bundles.ktor)
     testImplementation(libs.bundles.test)
