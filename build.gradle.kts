dd702dbf8408363f3a8a8985cc802d60f9f04f61
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-02 10:08:53 +0900

    [add] version catalogを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     kotlin("jvm") version "1.9.0"
     id("io.ktor.plugin") version "2.3.2"
@@ -17,9 +13,6 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktor)
+    testImplementation(libs.bundles.test)
 }
