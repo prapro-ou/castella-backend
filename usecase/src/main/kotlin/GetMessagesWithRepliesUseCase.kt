625755cfdc4eeb33a1b6b89cb52cda3e59c94d80
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-02 13:06:47 +0900

    [add] 特定のメッセージとその返信を返す機能を追加

---
+++ usecase/src/main/kotlin/GetMessagesWithRepliesUseCase.kt
@@ -0,0 +1,16 @@
+import com.vb4.result.ApiResult
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import message.MessageId
+import message.Message
+import message.MessageRepository
+
+class GetMessagesWithRepliesUseCase(
+    private val messageRepository: MessageRepository,
+    private val dispatcher: CoroutineDispatcher,
+) {
+    suspend operator fun invoke(messageId: MessageId): ApiResult<List<Message>, DomainException> =
+        withContext(dispatcher) {
+            messageRepository.getMessageWithReplies(messageId)
+        }
+}
