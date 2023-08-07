92bbac538c1d43ff4856c6f6fe8fb6a4b89bb83a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-08 00:50:27 +0900

    [add] MessageShowGetの返り値を追加

--- controller/src/main/kotlin/com/vb4/routing/destinations/messages/show/MessagesShowGet.kt
+++ controller/src/main/kotlin/com/vb4/routing/destinations/messages/show/MessagesShowGet.kt
@@ -10,7 +10,12 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import message.Message
 import message.MessageId
+import message.Reply
 import org.koin.ktor.ext.inject
 
 fun Route.messagesShowGet() {
@@ -20,12 +25,49 @@ fun Route.messagesShowGet() {
         call.getParameter<String>("messageId")
             .flatMap { messageId -> getMessageByIdUseCase(MessageId(messageId)) }
             .mapBoth(
-                success = { message -> message.id },
+                success = { messages -> MessagesShowGetResponse.from(messages) },
                 failure = { ExceptionSerializable.from(it) },
             )
             .consume(
                 success = { response -> call.respond(response) },
                 failure = { (exception, status) -> call.respond(status, exception.message) },
             )
+    }
+}
+
+@Serializable
+private data class MessagesShowGetResponse(
+    val messages: List<MessageSerializable>,
+) {
+    companion object {
+        fun from(message: Message) = MessagesShowGetResponse(
+            messages = MessageSerializable.from(message)
+        )
+    }
+}
+
+@Serializable
+private data class MessageSerializable(
+    val id: String,
+    val email: String,
+    val body: String,
+    @SerialName("created_at") val createdAt: Instant,
+) {
+    companion object {
+        fun from(message: Message) = listOf(
+            MessageSerializable(
+                id = message.id.value,
+                email = message.sender.email.value,
+                body = message.body.value,
+                createdAt = message.createdAt.value,
+            )
+        ) + message.replies.map { reply -> from(reply = reply) }
+
+        fun from(reply: Reply) = MessageSerializable(
+            id = reply.id.value,
+            email = reply.sender.email.value,
+            body = reply.body.value,
+            createdAt = reply.createdAt.value,
+        )
     }
 }
