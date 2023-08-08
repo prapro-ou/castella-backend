bebd7be993a010143c7aa0195465d63c5d12db1a
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-08 22:20:47 +0900

    [add] DevDBを追加

---
+++ data/src/main/kotlin/db/DevDB.kt
@@ -0,0 +1,41 @@
+package db
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
+import javax.sql.DataSource
+import repository.db.seeding.DatabaseSeeder
+
+val DevDB: Database by lazy {
+    val pool = hikari()
+    Database.connect(pool)
+        .also { pool.migrate() }
+        .also { it.seeding() }
+}
+
+private fun hikari(): HikariDataSource {
+    val config = HikariConfig().apply {
+        driverClassName = "org.h2.Driver"
+        jdbcUrl = "jdbc:h2:mem:dev_db;DB_CLOSE_DELAY=-1"
+        username = ""
+        password = ""
+        maximumPoolSize = MAXIMUM_POOL_SIZE
+        isAutoCommit = false
+        validate()
+    }
+    return HikariDataSource(config)
+}
+
+private fun DataSource.migrate() {
+    val flyway = Flyway.configure()
+        .dataSource(this)
+        .load()
+
+    flyway.info()
+    flyway.migrate()
+}
+
+fun Database.seeding() = listOf<DatabaseSeeder>().forEach { it.seeding(database = this) }
+
+private const val MAXIMUM_POOL_SIZE = 3
