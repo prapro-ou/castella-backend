3496d368868c9851e469f13b4f29f00ddda4c4d6
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-08 18:44:46 +0900

    [add] UserTableを追加

---
+++ data/src/main/kotlin/table/UsersTable.kt
@@ -0,0 +1,9 @@
+package repository.table
+
+import org.jetbrains.exposed.sql.Table
+
+object UsersTable : Table("users") {
+    val email = varchar("email", 256)
+
+    override val primaryKey: PrimaryKey = PrimaryKey(email)
+}
