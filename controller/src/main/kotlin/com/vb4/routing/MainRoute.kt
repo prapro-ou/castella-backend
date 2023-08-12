dee7660467780c6367ad5a58001d11f61db8116b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-12 18:26:52 +0900

    [add] Sucessルートを追加

--- controller/src/main/kotlin/com/vb4/routing/MainRoute.kt
+++ controller/src/main/kotlin/com/vb4/routing/MainRoute.kt
@@ -6,11 +6,15 @@ import com.vb4.routing.groups.show.groupsShowGet
 import com.vb4.routing.dms.show.messages.show.dmsShowMessagesShowGet
 import com.vb4.routing.groups.messages.show.groupsMessagesShowGet
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
 fun Application.mainRoute() {
     routing {
+        get("") { call.respond("Success") }
         route("destinations") { destinationsIndexGet() }
         route("dms") {
             dmsShowGet()
