e65fa9683ac9e546439f6a571721a4630d8d4bce
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-19 11:29:18 +0900

    [fix] アプリで必要とされている設定に変更

--- controller/src/main/kotlin/com/vb4/plugins/SessionPlugin.kt
+++ controller/src/main/kotlin/com/vb4/plugins/SessionPlugin.kt
@@ -1,16 +1,17 @@
 package com.vb4.plugins
 
+import com.vb4.user.User
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
-import kotlin.collections.set
+
+const val CASTELLA_SESSION = "castella_session"
 
 fun Application.configureSessionPlugin() {
-    data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+        cookie<CastellaSession>(CASTELLA_SESSION)
     }
 }
+
+data class CastellaSession(val authUser: User.AuthUser)
