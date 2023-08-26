1a4663579e73e98fa4ca46f6cdb818dcdc70dbb7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-08-26 17:19:36 +0900

    [delete] DMMessageRepositoryImplからImap要素を削除

--- data/src/main/kotlin/com/vb4/repository/DMMessageRepositoryImpl.kt
+++ data/src/main/kotlin/com/vb4/repository/DMMessageRepositoryImpl.kt
@@ -1,41 +1,30 @@
 package com.vb4.repository
 
 import com.vb4.DomainException
-import com.vb4.Email
-import com.vb4.NewMessageCount
-import com.vb4.avatar.Avatar
 import com.vb4.dm.DM
-import com.vb4.dm.DMBody
-import com.vb4.dm.DMCreatedAt
 import com.vb4.dm.DMMessage
 import com.vb4.dm.DMMessageId
 import com.vb4.dm.DMMessageRepository
 import com.vb4.dm.DMReply
-import com.vb4.dm.DMSubject
 import com.vb4.mail.imap.Imap
-import com.vb4.mail.imap.ImapMail
-import com.vb4.mail.imap.ImapMail.Companion.groupingOriginalToReply
 import com.vb4.mail.smtp.Smtp
 import com.vb4.mail.smtp.SmtpMail
-import com.vb4.repository.DMMessageDto.Companion.toDMMessageDto
-import com.vb4.repository.DMMessageDto.Companion.toDomain
 import com.vb4.result.ApiResult
 import com.vb4.runCatchWithContext
+import com.vb4.suspendTransactionAsync
 import db.table.DMMessagesTable
+import db.table.toDMMessage
+import db.table.toDMMessages
+import db.table.toDMReply
+import javax.mail.internet.InternetAddress
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import javax.mail.internet.InternetAddress
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
 import kotlinx.datetime.toJavaLocalDateTime
-import kotlinx.datetime.toKotlinLocalDateTime
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.batchInsert
+import org.jetbrains.exposed.sql.andWhere
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -47,58 +36,31 @@ class DMMessageRepositoryImpl(
 ) : DMMessageRepository {
     override suspend fun getDMMessages(dm: DM): ApiResult<List<DMMessage>, DomainException> =
         runCatchWithContext(dispatcher) {
-
-            val dbDMMessages = transaction(database) {
-                DMMessagesTable
-                    .select { DMMessagesTable.dmId eq dm.id.value }
-                    .toList()
-                    .map { it.toDMMessageDto() }
-            }
-
-            if(dbDMMessages.isEmpty()) {
-                val imapDMMessage = imap.search {
-                    dm(dm.userEmail.value, dm.to.email.value)
-                    dbDMMessages.forEach {
-                        not { messageId(it.id) }
-                    }
-                }
-                    .map { it.toDMMessageDto() }
-
-                transaction(database) {
-                    DMMessagesTable.batchInsert(imapDMMessage) {
-                        this[DMMessagesTable.id] = it.id
-                        this[DMMessagesTable.dmId] = dm.id.value
-                        this[DMMessagesTable.dmMessageId] = it.inReplyTo
-                        this[DMMessagesTable.from] = it.from
-                        this[DMMessagesTable.subject] = it.subject
-                        this[DMMessagesTable.body] = it.body
-                        this[DMMessagesTable.isRecent] = true
-                        this[DMMessagesTable.createdAt] = it.createdAt
-                            .toLocalDateTime(TimeZone.UTC)
-                            .toJavaLocalDateTime()
-                    }
-                }
-            }
-
-            return@runCatchWithContext transaction(database) {
+            transaction(database) {
                 DMMessagesTable
                     .select { DMMessagesTable.dmId eq dm.id.value }
                     .toList()
             }
-                .map { it.toDMMessageDto() }
-                .toDomain()
+                .toDMMessages()
         }
 
     override suspend fun getDMMessage(
         dm: DM,
         messageId: DMMessageId,
     ): ApiResult<DMMessage, DomainException> = runCatchWithContext(dispatcher) {
-        imap.search { dm(dm.userEmail.value, dm.to.email.value) }
-            .groupingOriginalToReply()
-            .first { (original, _) -> original.id == messageId.value }
-            .let { (original, replies) ->
-                original.toDMMessage(replies = replies.map { it.toDMReply() })
-            }
+        val original = suspendTransactionAsync(database) {
+            DMMessagesTable
+                .select { DMMessagesTable.dmId eq dm.id.value }
+                .andWhere { DMMessagesTable.id eq dm.id.value }
+                .first()
+        }
+        val replies = suspendTransactionAsync(database) {
+            DMMessagesTable
+                .select { DMMessagesTable.dmId eq dm.id.value }
+                .andWhere { DMMessagesTable.dmMessageId eq dm.id.value }
+                .toList()
+        }
+        original.await().toDMMessage(replies = replies.await().map { it.toDMReply() })
     }
 
     override suspend fun insertDMMessage(
@@ -106,35 +68,40 @@ class DMMessageRepositoryImpl(
         message: DMMessage,
     ): ApiResult<Unit, DomainException> = runCatchWithContext(dispatcher) {
         smtp.send(SmtpMail.from(dm, message))
+        transaction(database) {
+            DMMessagesTable.insert {
+                it[id] = message.id.value
+                it[dmId] = dm.id.value
+                it[dmMessageId] = null
+                it[from] = message.from.email.value
+                it[subject] = message.subject.value
+                it[body] = message.body.value
+                it[isRecent] = message.isRecent
+                it[createdAt] = message.createdAt.value.toLocalDateTime(TimeZone.UTC).toJavaLocalDateTime()
+            }
+        }
     }
 
     override suspend fun insertDMReply(
         dm: DM,
+        inReplyTo: DMMessageId,
         reply: DMReply,
     ): ApiResult<Unit, DomainException> = runCatchWithContext(dispatcher) {
-        smtp.send(SmtpMail.from(dm, reply))
+        smtp.send(SmtpMail.from(dm, inReplyTo, reply))
+        transaction(database) {
+            DMMessagesTable.insert {
+                it[id] = reply.id.value
+                it[dmId] = dm.id.value
+                it[dmMessageId] = inReplyTo.value
+                it[from] = reply.from.email.value
+                it[subject] = reply.subject.value
+                it[body] = reply.body.value
+                it[isRecent] = reply.isRecent
+                it[createdAt] = reply.createdAt.value.toLocalDateTime(TimeZone.UTC).toJavaLocalDateTime()
+            }
+        }
     }
 }
-
-private fun ImapMail.toDMMessage(replies: List<DMReply>) = DMMessage.reconstruct(
-    id = DMMessageId(id),
-    subject = DMSubject(subject),
-    body = DMBody(body),
-    createdAt = DMCreatedAt(createdAt),
-    from = Avatar.reconstruct(Email(from)),
-    isRecent = isRecent,
-    newMessageCount = NewMessageCount(replies.count { isRecent } + if (isRecent) 1 else 0),
-    replies = replies,
-)
-
-private fun ImapMail.toDMReply() = DMReply.reconstruct(
-    id = DMMessageId(id),
-    subject = DMSubject(subject),
-    body = DMBody(body),
-    createdAt = DMCreatedAt(createdAt),
-    isRecent = isRecent,
-    from = Avatar.reconstruct(Email(from)),
-)
 
 private fun SmtpMail.Companion.from(dm: DM, message: DMMessage) = SmtpMail(
     id = message.id.value,
@@ -144,75 +111,11 @@ private fun SmtpMail.Companion.from(dm: DM, message: DMMessage) = SmtpMail(
     body = message.body.value,
 )
 
-private fun SmtpMail.Companion.from(dm: DM, reply: DMReply) = SmtpMail(
+private fun SmtpMail.Companion.from(dm: DM, inReplyTo: DMMessageId, reply: DMReply) = SmtpMail(
     id = reply.id.value,
     from = InternetAddress(dm.userEmail.value),
     to = InternetAddress(dm.to.email.value),
-    inReplyTo = dm.to.email.value,
+    inReplyTo = inReplyTo.value,
     subject = reply.subject.value,
     body = reply.body.value,
 )
-
-data class DMMessageDto(
-    val id: String,
-    val from: String,
-    val subject: String,
-    val body: String,
-    val inReplyTo: String?,
-    val isRecent: Boolean,
-    val createdAt: Instant,
-) {
-
-    fun toDMMessage(replies: List<DMReply>): DMMessage = DMMessage.reconstruct(
-        id = DMMessageId(id),
-        subject = DMSubject(subject),
-        body = DMBody(body),
-        createdAt = DMCreatedAt(createdAt),
-        isRecent = isRecent,
-        from = Avatar.reconstruct(Email(from)),
-        newMessageCount = NewMessageCount(replies.count { isRecent } + if (isRecent) 1 else 0),
-        replies = replies,
-    )
-
-    fun toDMReply() = DMReply.reconstruct(
-        id = DMMessageId(id),
-        subject = DMSubject(subject),
-        body = DMBody(body),
-        createdAt = DMCreatedAt(createdAt),
-        isRecent = isRecent,
-        from = Avatar.reconstruct(Email(from)),
-    )
-
-    companion object {
-        fun ImapMail.toDMMessageDto() = DMMessageDto(
-            id = id,
-            from = from,
-            subject = subject,
-            body = body,
-            inReplyTo = inReplyTo,
-            isRecent = isRecent,
-            createdAt = createdAt,
-        )
-
-        fun ResultRow.toDMMessageDto() = DMMessageDto(
-            id = this[DMMessagesTable.id],
-            from = this[DMMessagesTable.from],
-            subject = this[DMMessagesTable.subject],
-            body = this[DMMessagesTable.body],
-            inReplyTo = this[DMMessagesTable.dmMessageId],
-            isRecent = this[DMMessagesTable.isRecent],
-            createdAt = this[DMMessagesTable.createdAt].toKotlinLocalDateTime().toInstant(TimeZone.UTC),
-        )
-
-        fun List<DMMessageDto>.toDomain(): List<DMMessage> {
-            val messages = this.groupBy { it.inReplyTo }
-            return (messages[null] ?: emptyList())
-                .fold(emptyList<Pair<DMMessageDto, List<DMMessageDto>>>()) { acc, original ->
-                    acc + (original to (messages[original.id] ?: emptyList()))
-                }
-                .map { (original, replies) ->
-                    original.toDMMessage(replies.map { it.toDMReply() })
-                }
-        }
-    }
-}
--- data/src/main/kotlin/db/table/DMMessagesTable.kt
+++ data/src/main/kotlin/db/table/DMMessagesTable.kt
@@ -1,8 +1,20 @@
 package db.table
 
+import com.vb4.Email
+import com.vb4.NewMessageCount
+import com.vb4.avatar.Avatar
+import com.vb4.dm.DMBody
+import com.vb4.dm.DMCreatedAt
+import com.vb4.dm.DMMessage
+import com.vb4.dm.DMMessageId
+import com.vb4.dm.DMReply
+import com.vb4.dm.DMSubject
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toKotlinLocalDateTime
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
-import org.jetbrains.exposed.sql.javatime.time
 
 object DMMessagesTable : Table("dm_messages") {
     val id = varchar("id", 256)
@@ -16,3 +28,34 @@ object DMMessagesTable : Table("dm_messages") {
 
     override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
+
+fun List<ResultRow>.toDMMessages(): List<DMMessage> {
+    val messages = this.groupBy { it[DMMessagesTable.dmMessageId] }
+    return (messages[null] ?: emptyList())
+        .fold(emptyList<Pair<ResultRow, List<ResultRow>>>()) { acc, original ->
+            acc + (original to (messages[original[DMMessagesTable.id]] ?: emptyList()))
+        }
+        .map { (original, replies) ->
+            original.toDMMessage(replies.map { it.toDMReply() })
+        }
+}
+
+fun ResultRow.toDMMessage(replies: List<DMReply>) = DMMessage.reconstruct(
+    id = DMMessageId(this[DMMessagesTable.id]),
+    from = Avatar.reconstruct(email = Email(this[DMMessagesTable.from])),
+    subject = DMSubject(this[DMMessagesTable.subject]),
+    body = DMBody(this[DMMessagesTable.body]),
+    isRecent = this[DMMessagesTable.isRecent],
+    newMessageCount = NewMessageCount(replies.count { it.isRecent } + if (this[DMMessagesTable.isRecent]) 1 else 0),
+    replies = replies,
+    createdAt = DMCreatedAt(this[DMMessagesTable.createdAt].toKotlinLocalDateTime().toInstant(TimeZone.UTC)),
+)
+
+fun ResultRow.toDMReply() = DMReply.reconstruct(
+    id = DMMessageId(this[DMMessagesTable.id]),
+    from = Avatar.reconstruct(email = Email(this[DMMessagesTable.from])),
+    subject = DMSubject(this[DMMessagesTable.subject]),
+    body = DMBody(this[DMMessagesTable.body]),
+    isRecent = this[DMMessagesTable.isRecent],
+    createdAt = DMCreatedAt(this[DMMessagesTable.createdAt].toKotlinLocalDateTime().toInstant(TimeZone.UTC)),
+)
